1f4ea0d70dc425c03bdec2a81a4898c5923043c2
Juan Carlos Gonzalez <juangblade@hotmail.com>
2022-11-11 13:53:45 -0300

    first commit

---
+++ components/NavbarCustom.jsx
@@ -0,0 +1,73 @@
+import React, { useState, useMemo } from "react";
+import {
+  Navbar,
+  Button,
+  Link,
+  Text,
+  useTheme,
+  Image,
+  Dropdown,
+} from "@nextui-org/react";
+const NavbarCustom = () => {
+  const [languaje, setLanguaje] = useState(new Set(["en"]));
+
+  const selectedValue = useMemo(
+    () => Array.from(languaje).join(", ").replaceAll("_", " "),
+    [languaje]
+  );
+
+  return (
+    <Navbar variant="sticky" className="navegacion">
+      <Navbar.Content hideIn="xs" variant="underline" css={{ maxH: "50px" }}>
+        <Navbar.Link href="tel: (787)279-1555" css={{ gap: "10px" }}>
+          <Image
+            src="/icons/phone.svg"
+            alt="icono-phone"
+            width={16}
+            height={16}
+          />
+          (787)279-1555
+        </Navbar.Link>
+        <Navbar.Link
+          href="mailto:miraflorestire@gmail.com"
+          css={{ gap: "10px" }}
+        >
+          <Image
+            src="/icons/mail.svg"
+            alt="icono-mail"
+            width={16}
+            height={16}
+          />
+          miraflorestire@gmail.com
+        </Navbar.Link>
+        <Navbar.Link href="#map" css={{ gap: "10px" }}>
+          <Image src="/icons/map.svg" alt="icono-map" width={16} height={16} />
+          Bayamon, Puerto Rico
+        </Navbar.Link>
+      </Navbar.Content>
+
+      <Navbar.Content hideIn="xs" variant="underline">
+        <Dropdown onChange={(e) => console.log(e)}>
+          <Dropdown.Button flat css={{ "&:hover": { color: "white" } }}>
+            {selectedValue == "en" ? "English" : "Español"}
+          </Dropdown.Button>
+          <Dropdown.Menu
+            aria-label="Static Actions"
+            disallowEmptySelection
+            selectionMode="single"
+            selectedKeys={languaje}
+            onSelectionChange={setLanguaje}
+            css={{
+              bg: "linear-gradient(to right, #FB7C35, #FB6535)",
+            }}
+          >
+            <Dropdown.Item key="en">English</Dropdown.Item>
+            <Dropdown.Item key="es">Español</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </Navbar.Content>
+    </Navbar>
+  );
+};
+
+export default NavbarCustom;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import "../styles/globals.scss";
+
+// 1. import `NextUIProvider` component
+import { NextUIProvider } from "@nextui-org/react";
+import { lightTheme } from "../themes/ligthTheme";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    // 2. Use at the root of your app
+    <NextUIProvider theme={lightTheme}>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
